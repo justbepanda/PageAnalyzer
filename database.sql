b2fcf382794aa9c59eea701a4db8e24c5d01fd8b
Yury Ustinov <justbepanda@gmail.com>
2024-06-11 14:50:31 +0300

    fix db

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     created_at TIMESTAMP
 );
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT REFERENCES urls(id) NOT NULL,
     status_code int,
